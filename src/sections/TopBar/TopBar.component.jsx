fdd7ca37677108d49ba0362e7d43762b641d6ae0
Ben Terry <beterry12@gmail.com>
2020-04-26 11:15:49 -0400

    init commit, top bar

---
+++ src/sections/TopBar/TopBar.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//import styles
+import styles from './TopBar.module.scss'
+
+export default () => (
+    <div className={styles.topbar}>
+        <div />
+    </div>
+)
